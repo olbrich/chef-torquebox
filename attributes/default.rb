785687ad53ed5becc645a44cd13d1ac7e9b0fd54
Kevin Olbrich <kolbrich@6fusion.com>
2012-03-09 12:01:53 -0500

    update for cr1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,8 @@
 
 
-default[:torquebox][:version] = "2.0.0.beta3"
+default[:torquebox][:version] = "2.0.0.cr1"
 default[:torquebox][:url] = "http://torquebox.org/release/org/torquebox/torquebox-dist/#{node[:torquebox][:version]}/torquebox-dist-#{node[:torquebox][:version]}-bin.zip"
-default[:torquebox][:checksum] = "ee4dedca5102566d5f07a9664bab144e775e1dcc"
+default[:torquebox][:checksum] = "0f3a4c8fb66203fef7079172621ab502"
+default[:torquebox][:jruby][:opts] = "--1.8"
+default[:torquebox][:backstage_gitrepo] = "git://github.com/torquebox/backstage.git"
+default[:torquebox][:backstage_home] = "/var/www/backstage"
