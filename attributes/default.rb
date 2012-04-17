ebbb2142033f0fd8f443719490e6961df6cf3c0d
Kevin Olbrich <kolbrich@6fusion.com>
2012-04-16 21:46:44 -0400

    update for torque box 2.0.1 release

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 
 
-default[:torquebox][:version] = "2.0.0.cr1"
+default[:torquebox][:version] = "2.0.1"
 default[:torquebox][:url] = "http://torquebox.org/release/org/torquebox/torquebox-dist/#{node[:torquebox][:version]}/torquebox-dist-#{node[:torquebox][:version]}-bin.zip"
-default[:torquebox][:checksum] = "0f3a4c8fb66203fef7079172621ab502"
+default[:torquebox][:checksum] = "7c37a5afae32af3fa07f9ba3b6c4eb07"
 default[:torquebox][:jruby][:opts] = "--1.8"
 default[:torquebox][:backstage_gitrepo] = "git://github.com/torquebox/backstage.git"
 default[:torquebox][:backstage_home] = "/var/www/backstage"
