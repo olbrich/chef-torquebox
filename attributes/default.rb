8b727e5ddd6b9d0e42a7126878828d195860449b
Kevin Olbrich <kevin.olbrich@gmail.com>
2012-05-04 15:15:04 -0300

    update for torquebox 2.0.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default[:torquebox][:version] = "2.0.1"
+default[:torquebox][:version] = "2.0.3"
 default[:torquebox][:url] = "http://torquebox.org/release/org/torquebox/torquebox-dist/#{node[:torquebox][:version]}/torquebox-dist-#{node[:torquebox][:version]}-bin.zip"
-default[:torquebox][:checksum] = "7c37a5afae32af3fa07f9ba3b6c4eb07"
+default[:torquebox][:checksum] = "c897b9a1a0225be6607fa68a6a952b0e"
 default[:torquebox][:jruby][:opts] = "--1.8"
 default[:torquebox][:backstage_gitrepo] = "git://github.com/torquebox/backstage.git"
 default[:torquebox][:backstage_home] = "/var/www/backstage"
